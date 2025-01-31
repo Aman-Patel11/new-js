8fd1d1e821a05536be6c92dc0c653c8005b42cf8
Aman Patel <amanpatel2580000@gmail.com>
2025-01-31 13:11:38 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";// treat all JS code as newer version
+
+
+alert ( 3 + 3 ) // we are using node,js, not browser
+
+// primitive datatypes
+// number
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// bifInt
+// symbol => unique
+
+// non-primitive datatypes
+// object
+
