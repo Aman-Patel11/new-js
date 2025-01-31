cf0565f239106ac6c2e59170cb3ab45866a89280
Aman Patel <amanpatel2580000@gmail.com>
2025-01-31 13:27:57 +0000

    typeconversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score); // typeconersion
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" =>33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn); //typeconversion
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+//"Aman" => true
+
+
